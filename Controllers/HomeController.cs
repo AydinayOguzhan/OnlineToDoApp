2c6e935c15faf4f46f22604b9fa80d4799b4d8c1
Oguzhan Aydinay <aydinayoguzhan@gmail.com>
2020-11-04 20:35:07 +0300

    Initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using OnlineToDo.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace OnlineToDo.Controllers
+{
+    public class HomeController : Controller
+    {
+        todoEntities db = new todoEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(users user)
+        {
+            var dbUser = db.users.FirstOrDefault(x => x.userName == user.userName && x.password == user.password);
+            if (dbUser != null)
+            {
+                FormsAuthentication.SetAuthCookie(dbUser.userName, false);
+                return RedirectToAction("NoteList","Todo");
+            }
+            else
+            {
+                ViewBag.HataMesaj = "Incorrect user name or password";
+                return View();
+            }
+        }
+
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(users user)
+        {
+            notes note = new notes();
+
+            if (!ModelState.IsValid)
+            {
+                ViewBag.CreateHata = "Confirm password doesn't match!";
+                return View("CreateUser");
+            }
+            else
+            {
+                db.users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ Controllers/TodoController.cs
@@ -0,0 +1,61 @@
+﻿using OnlineToDo.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace OnlineToDo.Controllers
+{
+    [Authorize]
+    public class TodoController : Controller
+    {
+        todoEntities db = new todoEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(notes note)
+        {
+            if (note.notes1 == null)
+            {
+                return RedirectToAction("NoteList", "Todo");
+            }
+            else
+            {
+                notes sNote = new notes();
+                sNote.notes1 = note.notes1;
+                sNote.username = User.Identity.Name;
+                db.notes.Add(sNote);
+                db.SaveChanges();
+
+                return RedirectToAction("NoteList", "Todo");
+            }
+        }
+
+        public ActionResult NoteList()
+        {
+            string u = User.Identity.Name;
+            var query = from b in db.notes where b.username == u select b;
+            return View(query);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var deleteModel = db.notes.Find(id);
+            if (deleteModel == null)
+            {
+                return HttpNotFound();
+            }
+            db.notes.Remove(deleteModel);
+            db.SaveChanges();
+            return RedirectToAction("NoteList", "Todo");
+        }
+    }
+}
